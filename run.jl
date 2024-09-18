20f1e39b624c9b92f017527b03d68ab08e32ae1e
Colton Kawamura <colton.kawamura1@nps.edu>
2024-09-17 20:38:27 -0700

    works with taking wall out and puting function in

--- run.jl
+++ run.jl
@@ -8,16 +8,23 @@ boxX = 1000
 boxY = 5.6896
 side = boxY
 cutoff = 1.4 # max particle diameter
-# box = Box([boxX,boxY], cutoff)
 k = 100
 gamma = 5
 
 particleList = convertInput("2D_N5000_P0.1_Width5_Seed1.mat")
 
-# cl = CellList([p.position for p in particleList], box)
+leftWall = [particle.position.x <= particle.diameter / 1.9 for particle in particleList] # added this to ID leftwall
+rightWall = [particle.position.x >= 1000 - particle.diameter / 2 for particle in particleList] # added this to ID leftwall 
+
+A = .1
+omega = 5
+moveLeftWall = (step, dt) -> A * sin(omega * step * dt)
+
 box, cl = makeBox(boxX, boxY, cutoff, particleList) 
 forceLaw = foo(k, gamma, box, cl)
 VelInitial = VelocitiesInit(particleList, 0, 1)
-trajectory = simAcou(particleList, VelInitial, 1, 0.001, 500, 10, forceLaw, side)
+
+trajectory = mdVerlet(particleList, VelInitial, 1, 0.001, 500, 10, forceLaw, side, group1=leftWall, group2=rightWall, moveFunc = moveLeftWall)
+
 plotTrajectoryAcoustic(trajectory)
 
--- src/simulate.jl
+++ src/simulate.jl
@@ -1,261 +1,40 @@
 export
-    md_verlet,
-    md_verlet_Acoustic,
-    md_verlet_AcousticCL,
-    md,
-    md_verletCL,
-    md_verlet_AcousticCL_2out,
-    md_verletCLosc,
-    md_verletAc,
-    sim,
-    simAcou
+    mdVerlet
 
-
-# method 1 without boundaries 
-function md_verlet(particle_list::Vector{Particle{VecType}}, VelInitial::Vector{VecType}, mass, dt, nsteps, save_interval, forces!, ForceLaw) where {VecType}
-    p = getfield.(particle_list, :position)  # extract just the positions as initial positions
-    v = copy(VelInitial)
-    a = similar(p)
-    f = similar(p)
-
-    trajectory = [(map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list))] 
- 
-    for step in 1:nsteps
-        forces!(f, particle_list, ForceLaw)
-        
-        a .= f ./ mass
-        p .= p .+ v .* dt .+ 0.5 .* a .* dt^2
-
-        setfield!.(particle_list, :position, p) # update positions
-        forces!(f, particle_list, ForceLaw) # update forces
-
-        a_new = f ./ mass
-        v .= v .+ 0.5 .* (a .+ a_new) .* dt
-
-        if mod(step, save_interval) == 0
-            println("Saved trajectory at step: ", step)
-            push!(trajectory, (map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list)))
-        end
-    end
-
-    return trajectory
-end
-
-# periodic method
-function md_verlet(particle_list::Vector{Particle{VecType}}, VelInitial::Vector{VecType}, mass, dt, nsteps, save_interval, forces!, ForceLaw, side) where {VecType}
-    p = getfield.(particle_list, :position)  # extract just the positions as initial positions
-    v = copy(VelInitial)
-    a = similar(p)
-    f = similar(p)
-
-    trajectory = [(map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list))] 
- 
-    for step in 1:nsteps
-        forces!(f, particle_list, ForceLaw)
-        
-        a .= f ./ mass
-        p .= p .+ v .* dt .+ 0.5 .* a .* dt^2
-
-        p .= periodic.(p, side)
-        setfield!.(particle_list, :position, p) # update positions
-        forces!(f, particle_list, ForceLaw) # update forces
-
-        a_new = f ./ mass
-        v .= v .+ 0.5 .* (a .+ a_new) .* dt
-
-        if mod(step, save_interval) == 0
-            println("Saved trajectory at step: ", step)
-            push!(trajectory, (map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list)))
-        end
-    end
-
-    return trajectory
-end
-
-# Acoustic simulation with oscillation. Works. No cell list so too slow.
-function md_verletAc(particleList::Vector{Particle{VecType}}, VelInitial::Vector{VecType}, mass, dt, nsteps, save_interval, forces!, ForceLaw, side) where {VecType}
-    p = getfield.(particleList, :position)  # extract just the positions as initial positions
-    v = copy(VelInitial)
-    a = similar(p)
-    f = similar(p)
-    p0 = copy(p)
-
-    trajectory = [(map(element -> copy(element.position), particleList), map(element -> element.diameter, particleList))]
-    A = .1 # added to to feed into sin
-    omega = 100 # added to feed into sin
-    leftIndex = [particle.position.x < particle.diameter / 2 for particle in particleList] # added this to ID leftwall
- 
- 
-    for step in 1:nsteps
-        forces!(f, particleList, ForceLaw)
-        
-        a .= f ./ mass
-        p .= p .+ v .* dt .+ 0.5 .* a .* dt^2
-        p[leftIndex] .= [Pos2D(p0[i][1] + A * sin(omega * step * dt), p0[i][2]) for i in findall(leftIndex)] # added update positions left wall
-
-
-        p .= periodic.(p, side)
-        setfield!.(particleList, :position, p) # update positions
-        forces!(f, particleList, ForceLaw) # update forces
-        f[leftIndex] .= [Pos2D(0.0, 0.0) for _ in findall(leftIndex)] # added to zero out forces on left wall
-
-        a_new = f ./ mass
-        v .= v .+ 0.5 .* (a .+ a_new) .* dt
-
-        if mod(step, save_interval) == 0
-            println("Saved trajectory at step: ", step)
-            push!(trajectory, (map(element -> copy(element.position), particleList), map(element -> element.diameter, particleList)))
-        end
-    end
-
-    return trajectory
-end
-
-
-
-# periodic method
-function md_verletCL(particle_list::Vector{Particle{VecType}}, VelInitial::Vector{VecType}, mass, dt, nsteps, save_interval, forces!, side) where {VecType}
-    p = getfield.(particle_list, :position)  # extract just the positions as initial positions
-    v = copy(VelInitial)
-    a = similar(p)
-    f = similar(p)
-
-    trajectory = [(map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list))] 
- 
-    for step in 1:nsteps
-        forces!(f, particle_list)
-        
-        a .= f ./ mass
-        p .= p .+ v .* dt .+ 0.5 .* a .* dt^2
-
-        p .= periodic.(p, side)
-        setfield!.(particle_list, :position, p) # update positions
-        forces!(f, particle_list) # update forces
-
-        a_new = f ./ mass
-        v .= v .+ 0.5 .* (a .+ a_new) .* dt
-
-        if mod(step, save_interval) == 0
-            println("Saved trajectory at step: ", step)
-            push!(trajectory, (map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list)))
-        end
-    end
-
-    return trajectory
-end
-
-
-
-# periodic method
-function md_verletCLosc(particle_list::Vector{Particle{VecType}}, VelInitial::Vector{VecType}, mass, dt, nsteps, save_interval, forces!, side) where {VecType}
+function mdVerlet(particle_list::Vector{Particle{VecType}}, VelInitial::Vector{VecType}, mass, dt, nsteps, save_interval, forces!, side; kwargs...) where {VecType}
     p = getfield.(particle_list, :position) 
-    p0 = copy(p) # added to get initial positions, used for the oscilation
+    p0 = copy(p)  # Added to get initial positions, used for the oscillation
     v = copy(VelInitial)
     a = similar(p)
     f = similar(p)
 
-    A = .01 # will move out function
-    omega = 10 # will move out function
-
     trajectory = [(map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list))] 
-    
-    leftIndex = [particle.position.x <= particle.diameter / 1.9 for particle in particle_list] # added this to ID leftwall
-    rightIndex = [particle.position.x >= 1000 - particle.diameter / 2 for particle in particle_list] # added this to ID leftwall 
-    
-    for step in 1:nsteps
-        forces!(f, particle_list)
 
-        a .= f ./ mass
-        p .= p .+ v .* dt .+ 0.5 .* a .* dt^2
-        p[leftIndex] .= [Pos2D(p0[i][1] + A * sin(omega * step * dt), p0[i][2]) for i in findall(leftIndex)] # added update positions left wall
-        p[rightIndex] .= [Pos2D(p0[i][1], p0[i][2]) for i in findall(rightIndex)] # added update positions left wall
+    # kwargs for groups
+    group1 = get(kwargs, :group1, nothing)
+    group2 = get(kwargs, :group2, nothing)
 
-        p .= periodic.(p, side)
-        setfield!.(particle_list, :position, p) 
-        forces!(f, particle_list) 
+    # Retrieve the move function for group1
+    moveFunc = get(kwargs, :moveFunc, (step, dt) -> 0.0)  # Default to no movement if moveFunc not passed
 
-        f[leftIndex] .= [Pos2D(0.0, 0.0) for _ in findall(leftIndex)] # added to zero out forces on left wall
-        f[rightIndex] .= [Pos2D(0.0, 0.0) for _ in findall(rightIndex)] # added to zero out forces on left wall
-
-        a_new = f ./ mass
-        v .= v .+ 0.5 .* (a .+ a_new) .* dt
-
-        if mod(step, save_interval) == 0
-            println("Saved trajectory at step: ", step)
-            push!(trajectory, (map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list)))
-        end
-    end
-
-    return trajectory
-end
-
-# simulatte
-function sim(particle_list::Vector{Particle{VecType}}, VelInitial::Vector{VecType}, mass, dt, nsteps, save_interval, forces!; kwargs...) where {VecType}
-    p = getfield.(particle_list, :position) 
-    p0 = copy(p) # added to get initial positions, used for the oscilation
-    v = copy(VelInitial)
-    a = similar(p)
-    f = similar(p)
-    trajectory = [(map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list))] 
-    # kwargs
-    side = get(kwargs, :side, nothing)
-    
     for step in 1:nsteps
         forces!(f, particle_list, v)
 
         a .= f ./ mass
         p .= p .+ v .* dt .+ 0.5 .* a .* dt^2
 
-        if haskey(kwargs, :side)
-            p .= periodic.(p, side)
-        end
+        # Move group1 based on the moveFunc
+        movement = moveFunc(step, dt)
+        p[group1] .= [Pos2D(p0[i][1] + movement, p0[i][2]) for i in findall(group1)]  # Move group1 according to moveFunc
 
-        setfield!.(particle_list, :position, p) 
-        forces!(f, particle_list, v) 
-
-
-        a_new = f ./ mass
-        v .= v .+ 0.5 .* (a .+ a_new) .* dt
-
-        if mod(step, save_interval) == 0
-            println("Saved trajectory at step: ", step)
-            push!(trajectory, (map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list)))
-        end
-    end
-
-    return trajectory
-end
-
-# periodic method
-function simAcou(particle_list::Vector{Particle{VecType}}, VelInitial::Vector{VecType}, mass, dt, nsteps, save_interval, forces!, side) where {VecType}
-    p = getfield.(particle_list, :position) 
-    p0 = copy(p) # added to get initial positions, used for the oscilation
-    v = copy(VelInitial)
-    a = similar(p)
-    f = similar(p)
-
-    A = .01 # will move out function
-    omega = 10 # will move out function
-
-    trajectory = [(map(element -> copy(element.position), particle_list), map(element -> element.diameter, particle_list))] 
-    
-    leftIndex = [particle.position.x <= particle.diameter / 1.9 for particle in particle_list] # added this to ID leftwall
-    rightIndex = [particle.position.x >= 1000 - particle.diameter / 2 for particle in particle_list] # added this to ID leftwall 
-    
-    for step in 1:nsteps
-        forces!(f, particle_list, v)
-
-        a .= f ./ mass
-        p .= p .+ v .* dt .+ 0.5 .* a .* dt^2
-        p[leftIndex] .= [Pos2D(p0[i][1] + A * sin(omega * step * dt), p0[i][2]) for i in findall(leftIndex)] # added update positions left wall
-        p[rightIndex] .= [Pos2D(p0[i][1], p0[i][2]) for i in findall(rightIndex)] # added update positions left wall
+        p[group2] .= [Pos2D(p0[i][1], p0[i][2]) for i in findall(group2)]  # Keep group2 fixed
 
         p .= periodic.(p, side)
-        setfield!.(particle_list, :position, p) 
-        forces!(f, particle_list, v) 
+        setfield!.(particle_list, :position, p)
+        forces!(f, particle_list, v)
 
-        f[leftIndex] .= [Pos2D(0.0, 0.0) for _ in findall(leftIndex)] # added to zero out forces on left wall
-        f[rightIndex] .= [Pos2D(0.0, 0.0) for _ in findall(rightIndex)] # added to zero out forces on left wall
+        f[group1] .= [Pos2D(0.0, 0.0) for _ in findall(group1)]  # Zero out forces on group1
+        f[group2] .= [Pos2D(0.0, 0.0) for _ in findall(group2)]  # Zero out forces on group2
 
         a_new = f ./ mass
         v .= v .+ 0.5 .* (a .+ a_new) .* dt
